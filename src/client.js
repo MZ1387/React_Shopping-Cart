1cd87709ba0eb9255587b55b041eada03c1dc9d4
Mario Zamora <maza1387@gmail.com>
2017-07-07 00:27:39 -0700

    Create and Test Mongoose API routes, Update postBooks Action to use axios, Use redux-thunk

--- src/client.js
+++ src/client.js
@@ -10,13 +10,14 @@ import { addToCart } from './actions/cartActions';
 import { postBooks, deleteBooks, updateBooks } from './actions/booksActions';
 
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import BooksList from './components/pages/booksList';
 import Cart from './components/pages/cart';
 import BooksForm from './components/pages/booksForm';
 import Main from './main';
 
-const middleware = applyMiddleware(logger);
+const middleware = applyMiddleware(thunk, logger);
 const store = createStore(reducers, middleware);
 
 const Routes = (
